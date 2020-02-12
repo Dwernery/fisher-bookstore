36f08ee4790ef9f74055ef8f81f43b7363ac92e9
David Wernery <david.wernery@gmail.com>
2020-02-12 14:15:56 -0500

    books page

---
+++ src/books/Books.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Books.css";
+
+export default function Book() {
+    return (
+        <div className="Home">
+            <div className = "lander">
+                <h1>Books</h1>
+                <p>Welcome to the books page.</p>
+            </div>
+        </div>
+    );
+}
